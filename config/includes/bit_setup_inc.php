110b69376be02a936aaf052b6cf11b37dafc4459
modela bitweaver <spiderr@bitweaver.org>
2021-04-15 12:14:54 -0400

    clean up for includes/ reorg

--- config/includes/bit_setup_inc.php
+++ config/includes/bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem;
 
 $registerHash = array(
 	'package_name' => 'config',
-	'package_path' => dirname( __FILE__ ).'/',
+	'package_path' => dirname( dirname( __FILE__ ) ).'/',
 	'required_package'=> TRUE,
 );
 $gBitSystem->registerPackage( $registerHash );
